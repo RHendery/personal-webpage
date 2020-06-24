c0fc5c595f72b80f5645f65326979d1eb2e33ad5
redhendery <redhendery@gmail.com>
2020-06-23 17:01:32 -0700

    add publications accordion and rough layout

--- src/components/Publications.js
+++ src/components/Publications.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React from 'react';
+import Books from './books/Books';
+import Papers from './papers/Papers';
 
 export default function Publications() {
   return (
     <div>
-      <h1 className="title is-1">Publications</h1>
-      <p>
-        I have written four books and published 19 papers.
-      </p>
+      <h1 className="title is-1">Books and Publications</h1>
+      <br />
+
+      <div className="columns">
+        <div className="column">
+          <h5 className="title is-5">Books</h5>
+          <Books />
+        </div>
+        <div className="column is-offset-2">
+          <h5 className="title is-5">Papers & Publications</h5>
+          <Papers />
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/books/Books.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Accordion from '../../layouts/Accordion';
+
+function Books() {
+ return (
+   <div>
+     <Accordion
+       title="Relative Clauses in Time and Space"
+       content="This monograph investigates the diachronic behaviour of relative clauses across a broad sample of constructions from genetically and geographically diverse languages. Previous studies of change in relative clause constructions have most frequently been restricted to individual languages or language families. By comparing such studies with each other and with the historical records of languages that have less commonly been the focus of diachronic syntactic works, I examine the strength of evidence for developments that are predicted by earlier literature to be 'natural' or even 'universal' pathways of change (for example, various sources of relative clause markers, the development of hypotaxis out of parataxis, shift from prenominal to postnominal relative clause position). I also examine the evidence for changes that synchronic typological studies of relative clause constructions might lead us to expect to find (i.e. diachronic variation in the same parameters by which relative clause types distinguish themselves synchronically)."
+     />
+     <Accordion
+       title="The Diachronic Typology of Relative Clauses"
+       content="This thesis investigates the diachronic behaviour of relative clauses across a broad sample of constructions from genetically and geographically diverse languages. Previous studies of change in relative clause constructions have most frequently been restricted to individual languages or language families. By comparing such studies with each other and with the historical records of languages that have less commonly been the focus of diachronic syntactic works, I examine the strength of evidence for developments that are predicted by earlier literature to be 'natural' or even 'universal'' pathways of change (for example, various sources of relative clause markers, the development of hypotaxis out of parataxis, shift from prenominal to postnominal relative clause position). I also look for evidence of changes that synchronic typological studies of relative clause constructions might lead us to expect to find (i.e., diachronic variation in the same parameters by which relative clause types distinguish themselves synchronically)."
+     />
+     <Accordion
+       title="Change in Kinship Systems"
+       content="Nothing to see here apparently"
+     />
+     <Accordion
+       title="Grammatical Change: Theory and Description"
+       content="This volume comprises a collection of papers on the theme of grammatical change that evolved out of a workshop sponsored by the Centre for Research on Language Change (The Australian National University). The papers extend the boundaries of what has been addressed under the label of ‘grammatical change’ by applying theories and models of grammatical change to new evidence; by illuminating the historical relationships between grammar and other levels of linguistics; and by extending the range of languages that have been examined from the perspective of grammatical change. Languages discussed include Murriny Patha, Walpiri, Gurindji, Walmajarri, and Kayardild, Lardil, Yukulta, English, Dutch, German, Afrikaans, French, Italian, Portuguese, Spanish, Serbo-Croatian, Bulgarian, Macedonian, Slovenian, Albanian, Greek, Old Church Slavonic, Tocharian, Mandarin, Cantonese, Quechua, Basque, and Tok Pisin."
+     />
+   </div>
+ );
+}
+
+export default Books;
