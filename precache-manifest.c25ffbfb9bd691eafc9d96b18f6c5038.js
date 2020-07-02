1dbc7c4c407dec9d9d53cbaa12bad792f7f64631
RHendery <hendery@gmail.com>
2020-07-02 17:04:32 +1000

    Updates

---
+++ precache-manifest.c25ffbfb9bd691eafc9d96b18f6c5038.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf21acbca98dd445feba9e664751077",
+    "url": "/hendery.net/index.html"
+  },
+  {
+    "revision": "73d11aad77b37620e80b",
+    "url": "/hendery.net/static/css/main.7767e1c2.chunk.css"
+  },
+  {
+    "revision": "07fbb205fc4806efe0fb",
+    "url": "/hendery.net/static/js/2.4e595b66.chunk.js"
+  },
+  {
+    "revision": "718fa98a469a94012f350cb6e7e01f06",
+    "url": "/hendery.net/static/js/2.4e595b66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73d11aad77b37620e80b",
+    "url": "/hendery.net/static/js/main.a72ab2e2.chunk.js"
+  },
+  {
+    "revision": "e497ee7dc5e3861313e1",
+    "url": "/hendery.net/static/js/runtime-main.783c170a.js"
+  },
+  {
+    "revision": "edcbcdc7acf156f7d242f76c4139b7ea",
+    "url": "/hendery.net/static/media/01_waves_of_words.edcbcdc7.jpg"
+  },
+  {
+    "revision": "0951e65fe5574ea7334d5b855f0ce813",
+    "url": "/hendery.net/static/media/02_Howitt_and_Fison.0951e65f.png"
+  },
+  {
+    "revision": "46ea67865d84b70db682a6d22872f6f0",
+    "url": "/hendery.net/static/media/03_AustKin.46ea6786.png"
+  },
+  {
+    "revision": "828312d9a51159ce7cf9edccb46a6e6f",
+    "url": "/hendery.net/static/media/04_MPCE.828312d9.jpg"
+  },
+  {
+    "revision": "ca06b0acccab3dcdec0c227cac978faa",
+    "url": "/hendery.net/static/media/05_Glossopticon_VR.ca06b0ac.jpg"
+  },
+  {
+    "revision": "099990cf33f3efb9e0fd813aac04199d",
+    "url": "/hendery.net/static/media/06_Barrawao.099990cf.png"
+  }
+]);
