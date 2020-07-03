73a53db74100bd74a06b2aaa359c50f1dc97e049
redhendery <redhendery@gmail.com>
2020-07-02 20:03:30 -0700

    Updates

---
+++ precache-manifest.2c23118ad6c7fcbd52c80e189bee9694.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ec179378bcc2afa4234e2975d32a5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "925a56f511b7651fa2e1",
+    "url": "/static/css/main.7767e1c2.chunk.css"
+  },
+  {
+    "revision": "07fbb205fc4806efe0fb",
+    "url": "/static/js/2.4e595b66.chunk.js"
+  },
+  {
+    "revision": "718fa98a469a94012f350cb6e7e01f06",
+    "url": "/static/js/2.4e595b66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "925a56f511b7651fa2e1",
+    "url": "/static/js/main.550d034d.chunk.js"
+  },
+  {
+    "revision": "f745a9eebbe41ea3676a",
+    "url": "/static/js/runtime-main.b0ada58a.js"
+  },
+  {
+    "revision": "edcbcdc7acf156f7d242f76c4139b7ea",
+    "url": "/static/media/01_waves_of_words.edcbcdc7.jpg"
+  },
+  {
+    "revision": "0951e65fe5574ea7334d5b855f0ce813",
+    "url": "/static/media/02_Howitt_and_Fison.0951e65f.png"
+  },
+  {
+    "revision": "46ea67865d84b70db682a6d22872f6f0",
+    "url": "/static/media/03_AustKin.46ea6786.png"
+  },
+  {
+    "revision": "828312d9a51159ce7cf9edccb46a6e6f",
+    "url": "/static/media/04_MPCE.828312d9.jpg"
+  },
+  {
+    "revision": "ca06b0acccab3dcdec0c227cac978faa",
+    "url": "/static/media/05_Glossopticon_VR.ca06b0ac.jpg"
+  },
+  {
+    "revision": "099990cf33f3efb9e0fd813aac04199d",
+    "url": "/static/media/06_Barrawao.099990cf.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hendery.net/precache-manifest.c25ffbfb9bd691eafc9d96b18f6c5038.js"
+  "/precache-manifest.2c23118ad6c7fcbd52c80e189bee9694.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hendery.net/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
